4389c3d69e578ae3623c71c010548ed566ba25ee
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2025-04-15 04:12:22 +0100

    ....

---
+++ src/app/About/page.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return 
+    <div>
+      <h1>About</h1>
+      <p>This is the about page of our Next.js application.</p>
+    </div>;
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function Profile() { 
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello Profile!
+      </h1>
+    </main>
+  )
+}
