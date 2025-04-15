37ce5f1df1f11389eb9d593b71979fb2f6d84b33
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2025-04-15 04:31:11 +0100

    ....

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+export default function FirstBlog() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello Blog First!
+      </h1>
+    </main>
+  )
+}
---
+++ src/app/blog/secord/page.tsx
@@ -0,0 +1,9 @@
+export default function SecondBlog() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello Blog Second!
+      </h1>
+    </main>
+  )
+}
